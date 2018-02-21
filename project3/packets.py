c631cba4952d95eb71a28cab0785f9098b7edc7a
xingyif <xing.yif@husky.neu.edu>
2018-02-20 22:08:03 -0500

    reading data from input

--- project3/packets.py
+++ project3/packets.py
@@ -2,35 +2,42 @@ import json
 
 class SendPacket:
 
-    def __init__(self, size, timeout, seq_num, data, eof):
-        self.size = size # int
-        self.timeout = timeout # time / long
+    def __init__(self, seq_num, data, size, timeout, ack, eof):
         self.seq_num = seq_num #int
         self.data = data #str
+        self.size = size #int
+        self.timeout = timeout # time / long
+        self.ack = ack #boolean
         self.eof = eof #boolean
     
     """
     converts this packet into a json format (presumably to be sent)
     @return String
     """
-    def get_json():
-        data_as_json = {'eof': eof, 'data': self.data, 'seq': self.seq, 'size': self.size}
+    def get_json(self):
+        data_as_json = {'seq_num': self.seq_num, 'data': self.data, 'size': self.size, 'timeout_time': self.timeout, 'ack': self.ack, 'eof': self.eof}
         return json.dumps(data_as_json)
         
     """
     returns True if this packet is timed out
     @return boolean
     """
-    def is_timed_out():
+    def is_timed_out(self):
         return time.time() > self.timeout
 
     """
     getter for seq number
     @return Integer
     """ 
-    def get_seq_num():
+    def get_seq_num(self):
         return self.seq_num
 
+    """
+    returns True is the data has been acknowledged
+    @return boolean
+    """
+    def is_acked(self):
+    		return self.ack
 
 
 class AckPacket:
@@ -42,7 +49,7 @@ class AckPacket:
     returns the ack number of this packet
     @return Integer
     """
-    def get_ack_num():
+    def get_ack_num(self):
         return self.ack_num
 
 
