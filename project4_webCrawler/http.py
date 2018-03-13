ae32a9387b40ea818f97d43a09be20db4500d043
Yifan Xing <xing.yif@DMCD25Q81D9FY14.tld>
2018-03-13 14:14:09 -0400

    implementing login post

--- project4_webCrawler/http.py
+++ project4_webCrawler/http.py
@@ -7,20 +7,26 @@ from HTMLParser import HTMLParser
 
 class Request:
     
-    def __init__(self, request_type, path, version, host, data=""):
+    def __init__(self, request_type, path, version, host, data="", token="", session_id=""):
         self.request_type = request_type
         self.path = path
         self.version = version
         self.host = host
         self.data = data
+        self.token = token
+        self.session_id = session_id
     
     """
     converts this request into a json format (presumably to be sent)
     @return String
     """
     def get_str(self):
-        request_as_str = "%s %s %s\nHost: %s\n\n%s" % (self.request_type, self.path, self.version, self.host, self.data)
-	return request_as_str
+        request_as_str = "%s %s %s\nHost: %s\n" % (self.request_type, self.path, self.version, self.host)
+        #request_as_str += "Cookie: csrftoken=%s; sessionid=%s" % (self.token, self.session_id, self.data)
+        request_as_str += "Cookie: csrftoken=%s\n" % (self.token)
+        #request_as_str += "Referer: http://fring.ccs.neu.edu/accounts/login/?next=/fakebook/\n" 
+        request_as_str += "\n\n%s" % self.data
+        return request_as_str
 
 
 
@@ -34,18 +40,39 @@ class Response:
 	    self.datetime = datetime
 	    self.length = length
 
+
 class PageParser(HTMLParser):
-    def __init__(self, csrf, urls, session_id):
+    def __init__(self):
+        HTMLParser.__init__(self)
         self.csrf = None
         self.urls = []
         self.session_id = None
 
     def handle_starttag(self, tag, attrs):
-        for attr in attrs:
-            print("attr: " + str(attr))
+        for attr, val in attrs:
+            pass
+            #print("attr: " + str(attr))
+            #print("val: " + str(val))
 
     def handle_endtag(self, tag):
         print("end tag: " + str(tag))
 
     def handle_data(self, data):
         print("Data: " + data)
+        if "csrftoken" in data:
+            self.csrf = data.split("csrftoken=")[1].split(";")[0]
+
+        if "sessionid" in data:
+            self.session_id = data.split("sessionid=")[1].split(";")[0]
+            
+
+
+
+
+
+
+
+
+
+
+
