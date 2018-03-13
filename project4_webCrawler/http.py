aab22ae23c449972f0eb0d5ebabc0add2452ade2
Yifan Xing <xing.yif@DMCD25Q81D9FY14.tld>
2018-03-13 17:04:34 -0400

    implemented handling flags

--- project4_webCrawler/http.py
+++ project4_webCrawler/http.py
@@ -15,17 +15,28 @@ class Request:
         self.data = data
         self.token = token
         self.session_id = session_id
-    
+
     """
-    converts this request into a json format (presumably to be sent)
+    constructs a get request
     @return String
     """
-    def get_str(self):
+    def get_request(self):
+        request_as_str = "%s %s %s\nHost: %s\n\n" % (self.request_type, self.path, self.version, self.host)
+        # request_as_str += "Cookie: csrftoken=%s; sessionid=%s\n\n&next=/fakebook/" % (self.token, self.session_id)
+        print "-----------------initial request-----------"
+        print request_as_str
+        return request_as_str
+    
+    """
+    constructs a post request
+    @return String
+    """
+    def post_request(self):
         request_as_str = "%s %s %s\nHost: %s\n" % (self.request_type, self.path, self.version, self.host)
         #request_as_str += "Cookie: csrftoken=%s; sessionid=%s" % (self.token, self.session_id, self.data)
-        request_as_str += "Cookie: csrftoken=%s;sessionid=%s" % (self.token, self.session_id)
+        request_as_str += "Cookie: csrftoken=%s;sessionid=%s\n\n" % (self.token, self.session_id)
         #request_as_str += "Referer: http://fring.ccs.neu.edu/accounts/login/?next=/fakebook/\n" 
-        request_as_str += "\n\n%s&next=/fakebook/" % self.data
+        request_as_str += "%s&next=/fakebook/" % self.data
         return request_as_str
 
 
@@ -45,25 +56,49 @@ class PageParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
         self.csrf = None
-        self.urls = []
+        self.urls_visited = []
+        self.urls_tovisit = []
         self.session_id = None
+        self.secret_flag_found = False
+        self.secret_flags = []
 
     def handle_starttag(self, tag, attrs):
-        for attr, val in attrs:
-            pass
-            #print("attr: " + str(attr))
-            #print("val: " + str(val))
+        # find links in the page
+        if tag == 'a':
+            for attr, val in attrs:
+                if attr == 'href':
+                    # only add to tovisit list if the url has not been visited
+                    # doesn't exist in tovisit list already, and it's valid
+                    if val not in urls_visited && val not in urls_tovisit && if "fring.ccs.neu.edu" in val:
+                        urls_tovisit.append(val)
+                        print("add a new valid url to visit: %s" % val)
+        # find secret flag
+        if tag == 'h2':
+            for attr, val in attrs:
+                if attr == 'class' && val == 'secret_flag':
+                    secret_flag = True
+
+
 
     def handle_endtag(self, tag):
-        print("end tag: " + str(tag))
+        #print("end tag: " + str(tag))
+        pass
 
     def handle_data(self, data):
-        print("Data: " + data)
+        #print("Data: " + data)
         if "csrftoken" in data:
             self.csrf = data.split("csrftoken=")[1].split(";")[0]
 
         if "sessionid" in data:
             self.session_id = data.split("sessionid=")[1].split(";")[0]
+
+        if secret_flag:
+            flag = data.split(" ")[1]
+            if flag not in secret_flags:
+                secret_flags.append(flag)
+                secret_flag_found = False
+                print "found one secret flag %s" % str(flag)
+
             
 
 
