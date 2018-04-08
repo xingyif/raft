d79a3cc9eda953daf7221d670c429ffcaf2da3c2
yifanxing <xing.yif@Husky.neu.edu>
2018-04-07 20:22:06 -0400

    implemented indefinitely resend RPC

--- project5_raft_distributed_system/try.py
+++ project5_raft_distributed_system/try.py
@@ -12,13 +12,33 @@
 #     thread.start()
 #     thread.join()
 #     print "thread finished...exiting"
+import random
 import threading, time
 
 
-def send_regular_heartbeat():
-    print("cur_time %s" % time.time())
-    print(time.ctime())
-    threading.Timer(0.15, send_regular_heartbeat).start()
+a = [0, 1, 2]
+
+def send_regular_heartbeat(a):
+    print("started sending again, a %s" % str(a))
+
+    a1 = a
+    t = threading.Timer(0.15, send_regular_heartbeat, [a1])
+    if len(a1) == 0:
+        t.cancel()
+        print("should cancel thread")
+    else:
+        for i in a:
+            print("cur_time %s" % time.time())
+            print(time.ctime())
+            d = random.randint(1, 10)
+            if (d == 2):
+                a1.pop()
+                print(len(a1))
+                print("popped something, d %d" % d)
+    t.start()
 
 
-send_regular_heartbeat()
+
+send_regular_heartbeat(a)
+print("didn't wait for this to finish")
+
